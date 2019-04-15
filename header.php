c1addabb5590b83163ceaed03beb81302223a728
Andy van Sundert <andy482382@gmail.com>
2019-04-15 10:23:34 +0200

    Added header and footer

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andy3
+ * Date: 15/04/2019
+ * Time: 10:21
+ */
+require 'config.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>FIFA</title>
+</head>
+<body>
