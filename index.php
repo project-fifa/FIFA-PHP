a312c705d1c71952c2910f70a89970f3a576a338
Sem van Elteren <sem407@outlook.com>
2019-04-16 11:16:56 +0200

    added PHP to index

--- index.php
+++ index.php
@@ -12,7 +12,15 @@ require 'header.php';?>
         <h1>Download de App!</h1>
         <img src="https://via.placeholder.com/550x350" alt="">
         <p>Wed op je favoriete team en word rijk!</p>
-        <button>Download</button>
+        <?php
+        if ($_SESSION == true){
+            ?><button>Download</button><?php
+        }
+        else {
+            ?><a href="login.php"><p>Login om te downloaden!</p></a><?php
+        }
+        ?>
+
 
     </div>
     <div class="new-team">
