108a31de21582ffca254fb8b9f4d5dbb155d8da6
Andy van Sundert <andy482382@gmail.com>
2019-04-18 10:10:29 +0200

    added forget password page, removed json files

---
+++ reset.php
@@ -0,0 +1,17 @@
+<?php
+require 'header.php';
+?>
+<div class="reset-wrapper">
+<div class="reset-div">
+
+    <h1>Wachtwoord Herstellen</h1>
+    <form action="" class="reset-form">
+        <input type="text" id="username-reset" placeholder="Gebruikersnaam">
+        <input type="text" id="email-reset" placeholder="E-mail">
+        <input type="text" id="password-reset" placeholder="Nieuwe Wachtwoord">
+        <input type="text" id="confirm-reset" placeholder="Herhaal Wachtwoord">
+        <input type="submit" value="Klaar!" id="resetButton">
+    </form>
+</div>
+
+</div>
