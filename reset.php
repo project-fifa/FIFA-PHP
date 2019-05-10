28307e4521553633ab959fe01ea1bc7d5b721de8
Carlo van den Elshout <D261759@edu.rocwb.nl>
2019-05-10 09:11:33 +0200

    reset password

--- reset.php
+++ reset.php
@@ -7,8 +7,7 @@ require 'header.php';
     <h1>Wachtwoord Herstellen</h1>
     <form action="loginController.php" class="reset-form" method="post">
         <input type="hidden" name="type" value="reset">
-        <input type="text" id="username-reset" placeholder="Gebruikersnaam">
-        <input type="text" id="email-reset" placeholder="E-mail">
+        <input type="text" id="username-reset" placeholder="Gebruikersnaam/E-mail">
         <input type="submit" value="Password reset" id="resetButton">
     </form>
 </div>
